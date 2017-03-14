4051da73909ccd861f8cb7fc7c6cbcb7a051ff1d
Jonas Zimmermann <dr.jonas@brown.edu>
2017-03-14 12:13:54 -0400

    add flag fields add more packet types

--- Cere2.lua
+++ Cere2.lua
@@ -151,6 +151,12 @@ function PktField:rangeGetter()
     return self._data_rng_getter[self.t]
 end
 
+FlagField = AField:new{
+    mask=0x00,
+    valuestring=nil,
+    ftype='flagfield',
+}
+
 CbPkt = klass:new{
     name='HEADER',
     fields={
@@ -207,7 +213,7 @@ function CbPkt:iterate(b_len)
         i = i + 1
         if i <= n then
             local f = self.fields[i]
-            if f.ftype=='afield' then
+            if f.ftype=='afield' or f.ftype=='flagfield' then
                 return i, buf_pos, 0, f, 1
             end
 
@@ -260,23 +266,23 @@ function CbPktGeneric:match(chid, type)
 end
 
 -- System heartbeat
-CbPktSysHeartbeat = CbPkt:new('cbPKT_SYSHEARTBEAT')
-function CbPktSysHeartbeat:match(chid, type)
-    return chid == self._conf_pkg_ch and type == 0x00
-end
+CbPktSysHeartbeat = CbPktConfig:new('cbPKT_SYSHEARTBEAT')
+CbPktSysHeartbeat.fields['type'].valuestring = {
+    [0x00] = "System Heartbeat cbPKTTYPE_SYSHEARTBEAT",
+}
 
 -- System protocol monitor
-CbPktSysProtocolMonitor = CbPkt:new('cbPKT_SYSPROTOCOLMONITOR',
+CbPktSysProtocolMonitor = CbPktConfig:new('cbPKT_SYSPROTOCOLMONITOR',
     {
         PktField:new{t='UINT32', n='sentpkts', d='Packets sent since last cbPKT_SYSPROTOCOLMONITOR (or 0 if timestamp=0)'},
     }
 )
-function CbPktSysProtocolMonitor:match(chid, type)
-    return chid == self._conf_pkg_ch and type == 0x01
-end
+CbPktSysProtocolMonitor.fields['type'].valuestring = {
+    [0x01] = "System Protocol Monitor PAcket",
+}
 
 -- System condition report packet
-CbPktSysInfo = CbPkt:new('cbPKT_SYSINFO',
+CbPktSysInfo = CbPktConfig:new('cbPKT_SYSINFO',
     {
         PktField:new{t='UINT32', n='sysfreq', d='System clock frequency in Hz', format='DEC'},
         PktField:new{t='UINT32', n='spikelen', d='The length of the spike events', format='DEC'},
@@ -294,18 +300,6 @@ CbPktSysInfo.fields['type'].valuestring = {
     [0x91] = "System set Spike Length cbPKTTYPE_SYSSETSPKLEN",
     [0x92] = "System set Runlevel cbPKTTYPE_SYSSETRUNLEV",
 }
-function CbPktSysInfo:match(chid, type)
-    local p_types = {
-        [0x10] = true,
-        [0x11] = true,
-        [0x12] = true,
-        [0x90] = true,
-        [0x91] = true,
-        [0x92] = true,
-    }
-    return chid == self._conf_pkg_ch and  p_types[type] ~= nil
-end
-
 
 -- System condition report packet
 CbPktSSModelSet = CbPktConfig:new('cbPKT_SS_MODELSET',
@@ -446,6 +440,157 @@ CbPktChanInfo.fields['type'].valuestring = {
     [0xCF] = "cbPKTTYPE_CHANSETAUTOTHRESHOLD",
 }
 
+-- File Config Information Packets
+CbPktFileCfg = CbPktConfig:new('cbPKT_FILECFG',
+    {
+        PktField:new{t='UINT32', n='options', d='File Config Option', format='HEX', valuestring={
+            [0x00]="Launch File dialog, set file info, start or stop recording cbFILECFG_OPT_NONE",
+            [0x01]="Keep-alive message cbFILECFG_OPT_KEEPALIVE",
+            [0x02]="Recording is in progress cbFILECFG_OPT_REC",
+            [0x03]="Recording stopped cbFILECFG_OPT_STOP",
+            [0x04]="NeuroMotive recording status cbFILECFG_OPT_NMREC",
+            [0x05]="Close file application cbFILECFG_OPT_CLOSE",
+            [0x06]="Recording datetime cbFILECFG_OPT_SYNCH",
+            [0x07]="Launch File dialog, do not set or do anything cbFILECFG_OPT_OPEN",
+        }},
+        -- FlagField:new{t='BOOLEAN', n='options.keepalive', format=32, mask=0x00000001, d='Keep-alive message cbFILECFG_OPT_KEEPALIVE', valuestring={'yes', 'no'}},
+        -- FlagField:new{t='BOOLEAN', n='options.rec', format=32, mask=0x00000002, d='Recording is in progress cbFILECFG_OPT_REC', valuestring={'yes', 'no'}},
+        -- FlagField:new{t='BOOLEAN', n='options.stop', format=32, mask=0x00000002, d='Recording is in progress cbFILECFG_OPT_REC', valuestring={'yes', 'no'}},
+        PktField:new{t='UINT32', n='duration', format='DEC'},
+        PktField:new{t='UINT32', n='recording', d='If cbFILECFG_OPT_NONE this option starts/stops recording remotely', format='HEX'},
+        PktField:new{t='UINT32', n='extctrl', d='If cbFILECFG_OPT_REC this is split number (0 for non-TOC). If cbFILECFG_OPT_STOP this is error code.', format='DEC_HEX'},
+        PktField:new{t='STRING', n='username', len=256},
+        PktField:new{t='STRING', n='filename', len=256},
+        PktField:new{t='STRING', n='comment', len=256},
+    }
+)
+CbPktFileCfg.fields['type'].valuestring = {
+    [0x61] = "File Config response cbPKTTYPE_REPFILECFG",
+    [0xE1] = "File Config request cbPKTTYPE_SETFILECFG",
+}
+
+-- Config All packet
+CbPktConfigAll = CbPktConfig:new('cbPKT_CONFIGALL')
+CbPktConfigAll.fields['type'].valuestring = {
+    [0x08] = "Config All Report cbPKTTYPE_REPCONFIGALL",
+    [0x88] = "Config All Request cbPKTTYPE_REQCONFIGALL",
+}
+
+-- Options for noise boundary packets
+CbPktSSNoiseBoundary = CbPktConfig:new('cbPKT_SS_NOISE_BOUNDARY',
+    {
+        PktField:new{t='UINT32', n='chan', d='channel being configured', format='DEC'},
+        PktField:new{t='FLOAT', n='afc', len=3, d='Center of ellipsoid'},
+        PktField:new{t='FLOAT', n='afS', len=9, d='Ellipsoid axes'},
+    }
+)
+CbPktSSNoiseBoundary.fields['type'].valuestring = {
+    [0x54] = "Noise boundary Report cbPKTTYPE_SS_NOISE_BOUNDARYREP",
+    [0xD4] = "Noise boundary Request cbPKTTYPE_SS_NOISE_BOUNDARYSET",
+}
+
+-- SS Statistics packets
+CbPktSSStatistics = CbPktConfig:new('cbPKT_SS_STATISTICS',
+    {
+        PktField:new{t='UINT32', n='nUpdateSpikes', d='update rate in spike counts', format='DEC'},
+        PktField:new{t='UINT32', n='nAutoalg', d='Sorting Algorithm', format='HEX', valuestring={
+            [0x00]="No sorting cbAUTOALG_NONE",
+            [0x01]="Auto spread cbAUTOALG_SPREAD",
+            [0x02]="Auto Hist Correlation cbAUTOALG_HIST_CORR_MAJ",
+            [0x03]="Auto Hist Peak Maj cbAUTOALG_HIST_PEAK_COUNT_MAJ",
+            [0x04]="Auto Hist Peak Fish cbAUTOALG_HIST_PEAK_COUNT_FISH",
+            [0x05]="Manual PCA cbAUTOALG_PCA",
+            [0x06]="Manual Hoops cbAUTOALG_HOOPS",
+            [0x07]="K-means PCA cbAUTOALG_PCA_KMEANS",
+            [0x08]="EM-clustering PCA cbAUTOALG_PCA_EM",
+            [0x09]="DBSCAN PCA cbAUTOALG_PCA_DBSCAN",
+        }},
+        PktField:new{t='UINT32', n='nMode', d='command to change sorting parameters', format='HEX', valuestring={
+            [0x00]="Change the settings and leave sorting the same cbAUTOALG_MODE_SETTING",
+            [0x01]="Change settings and apply this sorting to all channels cbAUTOALG_MODE_APPLY",
+        }},
+        PktField:new{t='FLOAT', n='fMinClusterPairSpreadFactor'},
+        PktField:new{t='FLOAT', n='fMaxSubclusterSpreadFactor'},
+
+        PktField:new{t='FLOAT', n='fMinClusterHistCorrMajMeasure'},
+        PktField:new{t='FLOAT', n='fMaxClusterPairHistCorrMajMeasure'},
+
+        PktField:new{t='FLOAT', n='fClusterHistValleyPercentage'},
+        PktField:new{t='FLOAT', n='fClusterHistClosePeakPercentage'},
+        PktField:new{t='FLOAT', n='fClusterHistMinPeakPercentage'},
+        PktField:new{t='UINT32', n='nWaveBasisSize', d='number of wave to collect to calculate the basis', format='DEC'},
+        PktField:new{t='UINT32', n='nWaveSampleSize', d='number of samples sorted with the same basis before re-calculating the basis', format='DEC'},
+
+    }
+)
+CbPktSSStatistics.fields['type'].valuestring = {
+    [0x55] = "SS Statistics Report cbPKTTYPE_SS_STATISTICSREP",
+    [0xD5] = "SS Statistics Request cbPKTTYPE_SS_STATISTICSSET",
+}
+
+
+-- SS Status packets
+CbPktSSStatus = CbPktConfig:new('cbPKT_SS_STATUS',
+    {
+        AField:new{n='cntlUnitStats'},
+        PktField:new{t='UINT32', n='cntlUnitStats.nMode', d='nMode', format='HEX', valuestring={
+            [0x00]="do not adapt at all",
+            [0x01]="always adapt",
+            [0x02]="adapt if timer not timed out",
+        }},
+        PktField:new{t='FLOAT', n='cntlUnitStats.fTimeOutMinutes', d='how many minutes until time out'},
+        PktField:new{t='FLOAT', n='cntlUnitStats.fElapsedMinutes', d='amount of time that has elapsed'},
+        AField:new{n='cntlNumUnits'},
+        PktField:new{t='UINT32', n='cntlNumUnits.nMode', d='nMode', format='HEX', valuestring={
+            [0x00]="do not adapt at all",
+            [0x01]="always adapt",
+            [0x02]="adapt if timer not timed out",
+        }},
+        PktField:new{t='FLOAT', n='cntlNumUnits.fTimeOutMinutes', d='how many minutes until time out'},
+        PktField:new{t='FLOAT', n='cntlNumUnits.fElapsedMinutes', d='amount of time that has elapsed'},
+    }
+)
+CbPktSSStatus.fields['type'].valuestring = {
+    [0x57] = "SS Status Report cbPKTTYPE_SS_STATUSREP",
+    [0xD7] = "SS Status Request cbPKTTYPE_SS_STATUSSET",
+}
+
+
+-- Sample Group Information packets
+CbPktGroupInof = CbPktConfig:new('cbPKT_GROUPINFO',
+    {
+        PktField:new{t='UINT32', n='proc',format='DEC'},
+        PktField:new{t='UINT32', n='group',format='DEC'},
+        PktField:new{t='STRING', n='label', len=cbConst.cbLEN_STR_LABEL},
+        PktField:new{t='UINT32', n='period', d='Sampling Period', format='DEC'},
+        PktField:new{t='UINT32', n='length', format='DEC'},
+        PktField:new{t='UINT32', n='list', lf='length', 'channelList'},
+    }
+)
+CbPktGroupInof.fields['type'].valuestring = {
+    [0x30] = "Sample Group Report cbPKTTYPE_GROUPREP",
+    [0xB0] = "Sample Group Request cbPKTTYPE_GROUPSET",
+}
+
+
+
+
+-- enum ADAPT_TYPE { ADAPT_NEVER, ADAPT_ALWAYS, ADAPT_TIMED };
+-- typedef struct {
+--     UINT32 nMode;           // 0-do not adapt at all, 1-always adapt, 2-adapt if timer not timed out
+--     float fTimeOutMinutes;  // how many minutes until time out
+--     float fElapsedMinutes;  // the amount of time that has elapsed
+--
+-- #ifdef __cplusplus
+--     void set(ADAPT_TYPE nMode, float fTimeOutMinutes)
+--     {
+--         this->nMode = static_cast<UINT32>(nMode);
+--         this->fTimeOutMinutes = fTimeOutMinutes;
+--     }
+-- #endif
+--
+-- } cbAdaptControl;
+
 
 -- Preview Stream
 CbPktStreamPrev = CbPkt:new('cbPKT_STREAMPREV',
@@ -543,52 +688,73 @@ function ProtoMaker:makeFieldsForPacket(pkt)
     local fn = pkt.name .. "_"
     self.fByPkt[pkt] = {}
     for i, f in ipairs(pkt.fields) do
-        if f.ftype~='afield' then
-            local thisfn = fn .. f.n
-            local thisn = n .. f.n
-            local pf = ProtoField.new(f.d and f.d or f.n, thisn, ftypes[f.t], f.valuestring, base[f.format], f.mask, f.d)
+        local thisfn = fn .. f.n
+        local thisn = n .. f.n
+        local pf
+
+        if f.ftype~='afield' and f.ftype~='flagfield' then
+            pf = ProtoField.new(f.d and f.d or f.n, thisn, ftypes[f.t], f.valuestring, base[f.format], f.mask, f.d)
+        elseif f.ftype=='flagfield' then
+            pf = ProtoField.new(f.d and f.d or f.n, thisn, ftypes[f.t], f.valuestring, f.format, f.mask, f.d)
+        end
+        if pf~=nil then
             self.pfields[thisfn] = pf
             self.fByPkt[pkt][f.n] = pf
+
             local df = Field.new(thisn)
             table.insert(pkt.dfields, df)
             pkt.dfields[f.n] = df
         end
     end
-     -- .. " " .. pkt.dfields[f.n] .. " " .. self.fByPkt[pkt])
 end
 
 function ProtoMaker:addSubtreeForPkt(buffer, tree, pkt)
-    local tree_stack = Stack:Create({t=tree, p=""})
+    local tree_stack = Stack:Create({t=tree, p="", br=buffer()})
 
     for i, bPos, width, pf, mult in pkt:iterate(buffer:len()) do
-        local current_parent = nil
+        local current_parent_tree = nil
+        local current_parent_br = nil
         repeat
             local last_tree = tree_stack:last()
             local i,j = string.find(pf.n, last_tree.p)
             if i==1 and j > 0 and string.sub(pf.n, j+1, j+1)=='.' then
-                current_parent = last_tree.t
+                current_parent_tree = last_tree.t
+                current_parent_br = last_tree.br
                 -- tree_stack:push(last_tree)
             elseif i==1 and j==0 then
-                current_parent = last_tree.t
+                current_parent_tree = last_tree.t
+                current_parent_br = last_tree.br
             else
                 tree_stack:pop()
             end
-        until current_parent ~= nil
+        until current_parent_tree ~= nil
         local subtree
+        local tvb_rng_wh
         if pf.ftype == 'afield' then
-            subtree = current_parent:add(pf.d ~= nil and pf.d or pf.n )
+            subtree = current_parent_tree:add(pf.d ~= nil and pf.d or pf.n )
+        elseif pf.ftype == 'flagfield' then
+            subtree = current_parent_tree:add_le(self.fByPkt[pkt][pf.n], current_parent_br)
         else
-            subtree = current_parent:add_le(self.fByPkt[pkt][pf.n], buffer(bPos, width))
-            if mult > 1 then
-                local n = mult - 1
-                subtree:append_text(" and "..n.." more item"..(n>1 and "s" or ""))
-                for i=1,n do
-                    local brng = buffer(bPos + width * i, width)
-                    subtree:add( brng, 'item '..i..': ' .. brng[pf:rangeGetter()](brng))
+            if mult > 0 then
+                tvb_rng_wh = buffer(bPos, width * mult)
+                local brng = buffer(bPos, width)
+                local this_rangeGetter = pf:rangeGetter()
+                if this_rangeGetter ~= nil then
+                    subtree = current_parent_tree:add_le(self.fByPkt[pkt][pf.n], tvb_rng_wh, brng[pf:rangeGetter()](brng) )
+                else
+                    subtree = current_parent_tree:add_le(self.fByPkt[pkt][pf.n], tvb_rng_wh)
+                end
+                if mult > 1 then
+                    local n = mult - 1
+                    subtree:append_text(" and "..n.." more item"..(n>1 and "s" or ""))
+                    for i=0,n do
+                        brng = buffer(bPos + width * i, width)
+                        subtree:add( brng, 'item '..i..': ' .. brng[pf:rangeGetter()](brng))
+                    end
                 end
             end
         end
-        tree_stack:push({t=subtree, p=pf.n})
+        tree_stack:push({t=subtree, p=pf.n, f=pf, br=tvb_rng_wh})
 
     end
 end
